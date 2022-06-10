ac9dc16517c0b20cc1f5b03bf735c1c27b81c458
valterelli45 <valter07082001@gmail.com>
2022-06-10 17:08:00 +0100

    changes

--- TrabalhoFinal/index.php
+++ TrabalhoFinal/index.php
@@ -9,8 +9,18 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
     <script src="bootstrap/js/bootstrap.js"></script>
     <script src="jquery/jquery-3.6.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
-    <h1>HELLO WORLD!</h1>
+<nav class="navbar">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="nav navbar-nav navbar-right">
+                <button type="submit" class="btn">Login</button>
+            </div>
+        </div>
+    </div>
+</nav>
 </body>
 </html>
