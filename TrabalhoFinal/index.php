75e52d0d4e02cdfe8e667c2aa5c5adb605720947
valterelli45 <valter07082001@gmail.com>
2022-06-11 00:07:30 +0100

    pull

--- TrabalhoFinal/index.php
+++ TrabalhoFinal/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Welcome Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
@@ -14,13 +14,39 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
-<nav class="navbar">
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <div class="nav navbar-nav navbar-right">
-                <button type="submit" class="btn">Login</button>
+    <section>
+        <nav class="navbar">
+            <div class="collapse navbar-collapse">
+                <div class="nav navbar-nav navbar-left">
+                    <img src="imgs/small_logo.jpg">
+                </div>
+                <div class="nav navbar-nav navbar-right">
+                    <button type="submit" class="btn">Login</button>
+                </div>
+            </div>   
+        </nav>
+
+<div class="container">
+    <div class="row">
+        <div class="row col-12">
+            <div>
+            <img class="img" src="imgs/logo.jpg"></img>
+            <h3>Dream Team</h3>
+            <h4>Bem vindos ao nosso website, aqui pode saber todos os dados dos seus jogadores de futebol favoritos! E para animar a sua estadia no nosso website desenvolvemos uma ferramenta do tipo "squad builder" em que pode juntar os seus jogadores favoritos para criar e guardar as suas "Dream Team's".</h4>
             </div>
         </div>
     </div>
-</nav>
+</div>
+
+<div class="footer">
+    <div class="row">
+        <hr>
+        Contact us on social networks!    
+        <i class="fab fa-facebook-f white-text"> </i>
+        <i class="fab fa-twitter white-text"> </i>
+        <i class="fab fa-instagram white-text"> </i>
+    </div>
+</div>
+</section>
 </body>
 </html>
