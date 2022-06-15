6327752e6337b601f25fb482c195292919821666
valterelli45 <valter07082001@gmail.com>
2022-06-15 23:41:34 +0100

    wd

--- TrabalhoFinal/profilepage.php
+++ TrabalhoFinal/profilepage.php
@@ -52,80 +52,25 @@
                 <div class="row col-12">
                     <div class="information">
                         <div class="username-header">
-                            <p class="text-left" style="color:white; margin-left:2%; font-size: 40px;">Darkgamer72</p>
+                            <p class="text-left" style="color:white; margin-left:2%; font-size: 35px;">Darkgamer72</p>
                         </div>
                         <div style="width: 120px; float: left; margin-top: 2%;">
                             <div class="user-sub-header" style="color:#838995;">Join Date</div>
                             <div class="user-sub-header-content" style="color:white;">17.01.19</div>
                         </div>
                         <div style="width: 120px; float:left; margin-top: 2%;">
-                            <div class="user-sub-header" style="color:#838995;">Comments</div>
-                            <div class="user-sub-header-content" style="color:white;">0</div>
-                        </div>
-                        <div style="width: 120px; float:left; margin-top: 2%;">
                             <div class="user-sub-header" style="color:#838995;">Squads</div>
                             <div class="user-sub-header-content" style="color:white;">0</div>
-                        </div>
-                        <div style="width: 120px; float:left; margin-top: 2%;">
-                            <div class="user-sub-header" style="color:#838995;">Votes</div>
-                            <div class="user-sub-header-content" style="color:white;">0</div>
-                        </div>
-                    </div>
-                    <div class="user-header ">
-                        <div class="user-header-content">
-                            <div class="container">
-                                <div class="classic-tabs ">
-                                    <ul class="nav" id="myClassicTab" role="tablist">
-                                        <li class="nav-item" style="width: 200px; float: left;">
-                                            <a class="nav-link waves-light active show waves-effect waves-light" id="activity-tab-classic" data-toggle="tab" href="#activity-classic" role="tab" aria-controls="activity-classic"><i class="fas fa-heart-rate "></i> Latest Activity</a>
-                                        </li>
-                                        <li class="nav-item" style="width: 200px; float: left;">
-                                            <a class="nav-link waves-light waves-effect waves-light" id="comments-tab-classic" data-toggle="tab" href="#comments-classic" role="tab" aria-controls="comments-classic"><i class="fas fa-comments "></i> Comments</a>
-                                        </li>
-                                        <li class="nav-item" style="width: 200px; float: left;">
-                                            <a class="nav-link waves-light waves-effect waves-light" id="votes-tab-classic" data-toggle="tab" href="#votes-classic" role="tab" aria-controls="votes-classic"><i class="fas fa-thumbs-up "></i> Player Votes</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                            <div class="user-sub-header-content"><button class="squadBtn btn">Go to my squads</button></div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="container pt-5">
                             <div class="row">
-                                <div class="col-md-8 p-0">
-                                    <div class="tab-content m_box p-0" id="myClassicTabContent" style="overflow: hidden;">
-                                        <div class="tab-pane " id="activity-classic" role="tabpanel" aria-labelledby="activity-tab-classic">
-                                            <div class="timeline-main py-4">
-                                                <ul class="timeline-light">
-                                                    <div class="row col-12 text-center justify-content-center">
-                                                        <i class="fas fa-thumbs-down mr-2 pt-1"></i> No activity was found
-                                                    </div>
-                                                </ul>
-                                            </div>
-                                        </div>
-
-                                        <div class="tab-pane " id="votes-classic" role="tabpanel" aria-labelledby="votes-tab-classic">
-                                            <div class="row py-3">
-                                                <div class="row col-12 text-center justify-content-center">
-                                                    <i class="fas fa-thumbs-down mr-2 pt-1"></i> No votes were found
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="tab-pane " id="comments-classic" role="tabpanel" aria-labelledby="comments-tab-classic">
-                                            <div class="row py-3">
-                                                <div class="row col-12 text-center justify-content-center">
-                                                    <i class="fas fa-thumbs-down mr-2 pt-1"></i> No comments were found
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
 
                                 <div class="col-md-4">
                                     <div class="m_box">
-                                        <div class="border-bottom font-weight-bold w-100 pb-1">Social Media Links</div>
+                                        <div class="border-bottom font-weight-bold w-100 pb-1"> Social Media Links</div>
                                         <div class="row mt-3">
                                             <div class="col-12 pl-0">
                                                 <i class="fab fa-twitter twitter-color"></i>
@@ -154,6 +99,7 @@
                                                 N\A
                                             </div>
                                         </div>
+                                        <button class="editS btn">Edit</button>
                                     </div>
                                 </div>
                             </div>
