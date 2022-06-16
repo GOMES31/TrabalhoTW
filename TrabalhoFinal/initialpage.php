7fd9ca44e1c7137cc723b6566ad83adef5c41b1c
valterelli45 <valter07082001@gmail.com>
2022-06-16 14:08:54 +0100

    e

--- TrabalhoFinal/initialpage.php
+++ TrabalhoFinal/initialpage.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Welcome</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="initialpage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
