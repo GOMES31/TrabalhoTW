168b472f2e4108b0bd5cc8f2398efecf097122b0
valterelli45 <valter07082001@gmail.com>
2022-06-17 03:10:04 +0100

    ddd

--- TrabalhoFinal/initialpage.php
+++ TrabalhoFinal/initialpage.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="initialpage.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
     <script src="bootstrap/js/bootstrap.js"></script>
     <script src="jquery/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
 </head>
+
 <body>
 <div id="loader"></div>
 <section>
@@ -34,41 +36,63 @@
                     </ul>
                 </li>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <div class="container">
-        <div class="row">
-            <div class="row col-12">
-                <img class="img" id="dreamteam-large" src="imgs/dreamteam_large.png">
-            <form class="navbar-form navbar-center" role="search">
-                <div class="form-group">
-                    <input id="searchInput" type="text" class="form-control" placeholder="Search for football players...">
-                    <button type="submit" id="searchIcon" class="fa fa-search"></button>
+        <div class="container">
+            <div class="row">
+                <div class="row col-12">
+                    <img class="img" id="dreamteam-large" src="imgs/dreamteam_large.png">
+                    <form class="navbar-form navbar-center" role="search">
+                        <div class="form-group">
+                            <input id="searchInput" type="text" class="form-control" placeholder="Search for football players...">
+                            <button type="submit" id="searchIcon" class="fa fa-search"></button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+                <div class="players">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="tableHeader text-center">Player Name</th>
+                                <th class="tableHeader text-center">Team</th>
+                                <th class="tableHeader text-center">Nationality</th>
+                                <th class="tableHeader text-center">Fav</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="player_search_1">
+                                <td>No player found</td>
+                                <td>No team found</td>
+                                <td>No nationality found</td>
+                                <td><button type="submit" id="starIcon" class="fa fa-star"></button></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                    <div class="col-md-12"></div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="footer">
-        <div class="row">
-            Contact us on social networks!
-            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
-            <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"> </i></a>
-            <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"> </i></a>
+        <div class="footer">
+            <div class="row">
+                Contact us on social networks!
+                <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"> </i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"> </i></a>
+            </div>
         </div>
-    </div>
-</section>
-<script>
-    let loader = document.getElementById("loader");
-    window.addEventListener("load",function (){
-        let delay = 1500;
+    </section>
+    <script>
+        let loader = document.getElementById("loader");
+        window.addEventListener("load", function() {
+            let delay = 1500;
 
-        setTimeout(function(){
-            loader.style.display = "none";
-        },delay);
-    });
-</script>
+            setTimeout(function() {
+                loader.style.display = "none";
+            }, delay);
+        });
+    </script>
 </body>
+
 </html>
